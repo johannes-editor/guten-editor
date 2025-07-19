8e732b7a584a486fb0d7264b0a78844483069c53
Alexandre <alexandretavares@outlook.com>
2025-07-19 16:55:33 -0300

    Define insertElementAfter, focusNextBlock, and focusOnElement methods in DomUtils class

--- src/utils/dom-utils.ts
+++ src/utils/dom-utils.ts
@@ -1,3 +1,4 @@
+
 export class DomUtils {
 
     static findClosestAncestorOfSelectionByClass(className: string): HTMLElement | null {
@@ -34,4 +35,74 @@ export class DomUtils {
         }
     }
 
+    static insertElementAfter(block: HTMLElement | null, newHTML: HTMLElement): HTMLElement {
+        const contentNode = document.getElementById("content")!;
+
+        if (block) {
+
+            block.insertAdjacentElement("afterend", newHTML);
+        } else {
+            contentNode.insertAdjacentElement("beforeend", newHTML);
+        }
+
+        return newHTML;
+    }
+
+    static focusNextBlock(currentBlock: HTMLElement | null) {
+        if (!currentBlock) return;
+
+        const next = currentBlock.nextElementSibling as HTMLElement | null;
+
+        if (next && next.classList.contains("block")) {
+            next.focus();
+
+            const range = document.createRange();
+            const selection = window.getSelection();
+
+            const firstTextNode = DomUtils.getFirstTextNode(next);
+            if (firstTextNode) {
+                range.setStart(firstTextNode, 0);
+            } else {
+                range.setStart(next, 0);
+            }
+
+            range.collapse(true);
+            selection?.removeAllRanges();
+            selection?.addRange(range);
+        }
+    }
+
+    static focusOnElement(currentBlock: HTMLElement | null) {
+        if (!currentBlock) return;
+
+        currentBlock.focus();
+
+        const range = document.createRange();
+        const selection = window.getSelection();
+
+        const firstTextNode = DomUtils.getFirstTextNode(currentBlock);
+        if (firstTextNode) {
+            range.setStart(firstTextNode, 0);
+        } else {
+            range.setStart(currentBlock, 0);
+        }
+
+        range.collapse(true);
+        selection?.removeAllRanges();
+        selection?.addRange(range);
+
+    }
+
+    static getFirstTextNode(element: HTMLElement): Text | null {
+        const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, {
+            acceptNode: (node) => {
+                if (node.textContent?.trim().length) {
+                    return NodeFilter.FILTER_ACCEPT;
+                }
+                return NodeFilter.FILTER_SKIP;
+            }
+        });
+
+        return walker.nextNode() as Text | null;
+    }
 }
