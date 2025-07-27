5d5b69399039bb567cd15656bd935e9682eba81c
Alexandre <alexandretavares@outlook.com>
2025-07-27 18:38:31 -0300

    Update demo

--- demo/loader.ts
+++
@@ -1,7 +0,0 @@
-// // demo/main.ts
-// import { initEditor } from "../src/main.tsx";
-
-// const root = document.getElementById("editor")!;
-// initEditor(root);
-
-export { initEditor } from "../src/main.tsx";
