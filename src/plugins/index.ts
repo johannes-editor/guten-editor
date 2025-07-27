da1a64386e9580b5c633100156bc3c1e8ac06399
Alexandre <alexandretavares@outlook.com>
2025-07-26 21:53:09 -0300

    Add plugins/index.ts to re-export core APIs/types, giving plugins a single entry point and avoiding deep relative paths

---
+++ src/plugins/index.ts
@@ -0,0 +1,4 @@
+export { h, Fragment } from "../jsx.ts";
+export { t, registerTranslation } from "../core/i18n/index.ts";
+export type { TranslationSchema } from "../core/i18n/types.ts";
+export { DomUtils } from "../utils/dom-utils.ts";
