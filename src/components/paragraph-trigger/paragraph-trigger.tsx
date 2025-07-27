760b9b262d77bce16184d3abcef57a9abfa8ec33
Alexandre <alexandretavares@outlook.com>
2025-07-26 21:49:19 -0300

    Add ParagraphTrigger: clicking below the content area inserts a ParagraphFn and focuses it

---
+++ src/components/paragraph-trigger/paragraph-trigger.tsx
@@ -0,0 +1,33 @@
+/** @jsx h */
+
+import { h } from "../../jsx.ts";
+import { EventTypes } from "../../constants/event-types.ts";
+import { Component } from "../component.ts";
+import { appendElementOnContentArea } from "../../core/editor-engine/index.ts";
+import { ParagraphFn } from "../blocks/paragraph-fn.tsx";
+import { DomUtils } from "../../utils/dom-utils.ts";
+
+export class ParagraphTrigger extends Component {
+
+    static override styles = /*css*/ `
+        #paragraphTriggerArea {
+            height: 3rem;
+            width: 100%;
+        }
+    `;
+
+    override onMount(): void {
+        this.registerEvent(this, EventTypes.Click, this.handleClick)
+    }
+
+    private readonly handleClick = () => {
+
+        const element = appendElementOnContentArea(<ParagraphFn />)
+        DomUtils.focusOnElement(element);
+        ;
+    };
+
+    override render(): HTMLElement {
+        return (<div id="paragraphTriggerArea"></div>);
+    }
+}
