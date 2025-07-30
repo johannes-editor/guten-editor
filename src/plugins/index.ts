8cb9c8a2610200201b137bd7eb49d605d771add7
Alexandre <alexandretavares@outlook.com>
2025-07-30 20:02:01 -0300

    Exposes h, Fragment, i18n helpers, DomUtils, Plugin, and related types through the plugins index to establish a unified public API

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,4 +1,7 @@
 export { h, Fragment } from "../jsx.ts";
 export { t, registerTranslation } from "../core/i18n/index.ts";
-export type { TranslationSchema } from "../core/i18n/types.ts";
 export { DomUtils } from "../utils/dom-utils.ts";
+export { Plugin } from "../core/plugin-engine/plugin.ts";
+
+export type { TranslationSchema } from "../core/i18n/types.ts";
+export type { DefaultProps } from "../components/types.ts";
