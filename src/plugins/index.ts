358cfe809908ec85fb0c1ada1bc63babf0f10082
Alexandre <alexandretavares@outlook.com>
2025-08-01 19:10:58 -0300

    Organize and expand plugin API exports

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,30 @@
+/* jsx */
 export { h, Fragment } from "../jsx.ts";
-export { t, registerTranslation } from "../core/i18n/index.ts";
-export { DomUtils } from "../utils/dom-utils.ts";
-export { Plugin } from "../core/plugin-engine/plugin.ts";
-export type { TranslationSchema } from "../core/i18n/types.ts";
+
+/* components */
+export { Component } from "../components/component.ts";
+export { OverlayComponent } from "../components/overlay/overlay-component.ts";
 export type { DefaultProps } from "../components/types.ts";
+
+/* translations */
+export type { TranslationSchema } from "../core/i18n/types.ts";
+export { t, registerTranslation } from "../core/i18n/index.ts";
+
+/* dom utilities */
+export { focusOnElement } from "../utils/dom-utils.ts";
+export { debounce } from "../utils/utils.ts";
+
+
+export { EventTypes } from "../constants/event-types.ts";
+export { KeyboardKeys } from "../constants/keyboard-keys.ts";
+
+/* plugins development */
+export { Plugin } from "../core/plugin-engine/plugin.ts";
+export { ExtensiblePlugin } from "../core/plugin-engine/extensible-plugin.ts";
+export { PluginExtension } from "../core/plugin-engine/plugin-extension.ts";
+
+/* editor utilities */
+export { appendElementOnOverlayArea } from "../core/editor-engine/index.ts";
+
+
+
