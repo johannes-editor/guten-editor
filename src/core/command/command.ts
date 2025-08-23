b1bf7e23df34a7237be3b0284f99aba58ba41559
Alexandre <alexandretavares@outlook.com>
2025-08-23 18:46:25 -0300

    add new fields to CommandContext

--- src/core/command/command.ts
+++ src/core/command/command.ts
@@ -1,16 +1,23 @@
+import { Plugin } from "../../plugins/index.ts";
 
 export interface CommandContext {
-  root: HTMLElement;
-  selection: Selection;
+    root?: HTMLElement;
+    selection?: Selection;
+    url?: string;
+    event?: Event;
+    // deno-lint-ignore no-explicit-any
+    target?: any;
+    latex?: string;
+    displayMode?: boolean;
 }
 
 export interface Command {
-  id: string;
-  execute(context?: CommandContext): boolean;
+    id: string;
+    execute(context?: CommandContext): boolean;
 }
 
 export class CommandRegistry {
-  private map = new Map<string, Command>();
-  register(cmd: Command) { this.map.set(cmd.id, cmd); }
-  run(id: string, context?: CommandContext) { return this.map.get(id)?.execute(context) ?? false; }
+    private map = new Map<string, Command>();
+    register(cmd: Command) { this.map.set(cmd.id, cmd); }
+    run(id: string, context?: CommandContext) { return this.map.get(id)?.execute(context) ?? false; }
 }
