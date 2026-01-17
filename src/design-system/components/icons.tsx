2e6267bb5bc4da90d84c9fbd5c3b2d9bdd4af52e
Alexandre <alexandretavares@outlook.com>
2026-01-17 16:49:01 -0300

    update equation icon

--- src/design-system/components/icons.tsx
+++ src/design-system/components/icons.tsx
@@ -102,7 +102,7 @@ export const LinkMaterialIcon = () => (
 
 export const EquationIcon = () => (
 
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 256 256"><path d="M180,72V60H89l48.4,60.5a12,12,0,0,1,0,15L89,196h91V184a12,12,0,0,1,24,0v24a12,12,0,0,1-12,12H64a12,12,0,0,1-9.37-19.5l58-72.5-58-72.5A12,12,0,0,1,64,36H192a12,12,0,0,1,12,12V72a12,12,0,0,1-24,0Z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-math-integral-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 19a2 2 0 0 0 2 2c2 0 2 -4 3 -9s1 -9 3 -9a2 2 0 0 1 2 2" /><path d="M14 12l6 6" /><path d="M14 18l6 -6" /></svg>
 
 );
 
