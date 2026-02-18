276083dc488c78c1301b919320d3e49feea97fe2
Alexandre <alexandretavares@outlook.com>
2026-02-18 20:56:59 -0300

    Update css.d.ts

--- src/types/css.d.ts
+++ src/types/css.d.ts
@@ -2,8 +2,3 @@ declare module "*.css" {
     const css: string;
     export default css;
 }
-
-// declare module "*.css?*" {
-//     const css: string;
-//     export default css;
-// }
--- src/types/import-meta.d.ts
+++
@@ -1,9 +0,0 @@
-// import { ImportGlobFunction } from "npm:vite";
-
-// export {};
-
-// declare global {
-//   interface ImportMeta {
-//     glob: ImportGlobFunction;
-//   }
-// }
