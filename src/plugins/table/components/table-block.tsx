ba34016452ca744ceb8647181ea755904046b0b6
Alexandre <alexandretavares@outlook.com>
2026-02-21 19:27:04 -0300

    Update table block

--- src/plugins/table/components/table-block.tsx
+++ src/plugins/table/components/table-block.tsx
@@ -1,18 +1,22 @@
-export function TableBlock(): HTMLTableElement {
+import { DefaultProps } from "@core/components";
+
+export function TableBlock(props: DefaultProps): HTMLTableElement {
     return (
         <div className="block table-block" contentEditable="false">
-            <table>
-                <tbody>
-                    <tr>
-                        <td contentEditable="true">{"\u00A0"}<br /></td>
-                        <td contentEditable="true">{"\u00A0"}<br /></td>
-                    </tr>
-                    <tr>
-                        <td contentEditable="true">{"\u00A0"}<br /></td>
-                        <td contentEditable="true">{"\u00A0"}<br /></td>
-                    </tr>
-                </tbody>
-            </table>
+            {props.children ?? (
+                <table>
+                    <tbody>
+                        <tr>
+                            <td contentEditable="true">{"\u00A0"}<br /></td>
+                            <td contentEditable="true">{"\u00A0"}<br /></td>
+                        </tr>
+                        <tr>
+                            <td contentEditable="true">{"\u00A0"}<br /></td>
+                            <td contentEditable="true">{"\u00A0"}<br /></td>
+                        </tr>
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 }
