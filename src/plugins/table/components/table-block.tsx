79f0b8df03b73d24046edd507e6566cde275004a
Alexandre <alexandretavares@outlook.com>
2026-02-21 19:21:05 -0300

    Refactor table block

--- src/plugins/table/components/table-block.tsx
+++ src/plugins/table/components/table-block.tsx
@@ -1,51 +1,18 @@
-import { DefaultProps } from "@core/components";
-import { runCommand } from "@core/command";
-import { generateBlockId } from "@utils/dom";
-
-export function TableBlock({ children, className = "", ...props }: DefaultProps): HTMLTableElement {
-    const blockClass = ["block", "table-block", className].filter(Boolean).join(" ");
-    const content = children ?? (
-        <table>
-            <tbody>
-                <tr>
-                    <td contentEditable="true">{"\u00A0"}<br /></td>
-                    <td contentEditable="true">{"\u00A0"}<br /></td>
-                </tr>
-                <tr>
-                    <td contentEditable="true">{"\u00A0"}<br /></td>
-                    <td contentEditable="true">{"\u00A0"}<br /></td>
-                </tr>
-            </tbody>
-        </table>
-    );
-
+export function TableBlock(): HTMLTableElement {
     return (
-        <div
-            className={blockClass}
-            data-block-id={generateBlockId()}
-            contentEditable="false"
-            {...props}
-        >
-            {content}
-
-            <div className="table-add-row-area">
-                <button type="button" className="table-add-row" onClick={handleAddRow}>
-                    +
-                </button>
-            </div>
-            <div className="table-add-column-area">
-                <button type="button" className="table-add-column" onClick={handleAddColumn}>
-                    +
-                </button>
-            </div>
+        <div className="block table-block" contentEditable="false">
+            <table>
+                <tbody>
+                    <tr>
+                        <td contentEditable="true">{"\u00A0"}<br /></td>
+                        <td contentEditable="true">{"\u00A0"}<br /></td>
+                    </tr>
+                    <tr>
+                        <td contentEditable="true">{"\u00A0"}<br /></td>
+                        <td contentEditable="true">{"\u00A0"}<br /></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
-}
-
-function handleAddRow(event: Event) {
-    runCommand("table.addRow", { event });
-}
-
-function handleAddColumn(event: Event) {
-    runCommand("table.addColumn", { event });
 }
